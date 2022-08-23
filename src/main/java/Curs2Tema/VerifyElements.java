dbbdac824475171c180188bc399f22417cd7aa8d
Gabi <gabigavan@yahoo.com>
2022-08-23 10:51:17 +0300

    Curs2Tema

---
+++ src/main/java/Curs2Tema/VerifyElements.java
@@ -0,0 +1,37 @@
+package Curs2Tema;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import Utils.BaseTest;
+
+public class VerifyElements extends BaseTest{
+
+	@Test
+	public void verifyElementsExist() throws InterruptedException {
+		
+		//verificam daca elementul e vizibil inainte de click pe login
+		WebElement logingemail = driver.findElement(By.id("log"));
+		System.out.println("Elementul Login or Email este present inainte de click pe login:" +logingemail.isDisplayed());
+		
+		//verificam daca elementul e vizibil inainte de click pe login
+		WebElement password = driver.findElement(By.id("password"));
+		System.out.println("Elementul Password este present inainte de click pe login:" +password.isDisplayed());
+		
+		//accesam login
+		WebElement login =driver.findElement(By.className("menu_user_login"));
+		
+		System.out.println("Elementul login este present:" +login.isDisplayed());
+		
+		login.click();  
+		
+		//verificam daca elementul e vizibil dupa click pe login
+		System.out.println("Elementul Login or Email este present dupa click pe login:" +logingemail.isDisplayed());
+				
+		//verificam daca elementul e vizibil dupa click pe login
+		System.out.println("Elementul Password este present dupa click pe login:" +password.isDisplayed());
+		
+			
+	}
+}
